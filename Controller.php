2951c6d863bea3fc8ce08a2e4bc523763e7684b6
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-09-14 17:01:31 +0200

    Update Controller abstract class

--- Controller.php
+++ Controller.php
@@ -16,27 +16,74 @@
      
     public function setRequest(Request $request)
     {
-
-        $this->request = $request;
-
+     $this->request = $request;
     }
 
     /**
-     * Execute l'action
+     * Exécute l'action
      * 
-     * @throws \Exception s'il y a une erreur
+     * @throws \Exception erreur si l'action n'existe pas dans la classe Controller
      */
 
     public function executeAction($action) 
     {
 
-  
+        if (method_exists($this, $action)) //Vérifie si un méthode nommée $action existe dans la classe
+        {
 
+         $this->action = $action;
+         $this->{$this->action}(); //Appel de la méthode nommée $action
+
+        }
+        else
+        {
+
+         $classController = get_class($this);
+         throw new \Exception ("Action '$action' non définie dans la classe '$classController'")  
+
+        }
 
     }
 
+    /**
+     * Action par défaut index()
+     * La méthode abstraite oblige les classes dérivées à l'implémenter
+     */
 
+    public abstract function index();
 
+    /**
+     * 
+     * Génération de la vue associée au controleur avec la méthode generateView()
+     * @param $dataView stocke dans un tableau les données pour la génération de la vue  
+     *  
+     * */
+    
+     protected function generateView($dataView = array())
+     {
+        //Détermine le nom du fichier vue à partir du nom du controleur
+        $classController = get_class($this);
+        $controller = str_replace("Controller", "", $classController);
+        $view = new View($this->action, $controller);
+        $view->generate($dataView);         
+
+     }
+
+     /**
+      * Redirige vers un controleur et une action spécifiques 
+      *
+      * @param string $controller Controller
+      * @param string $action Action
+      */
+
+      protected function redirect($controller, $action = null)
+      {
+
+       $rootWeb = Config::get("rootWeb", "/");
+       //Création de l'URL racine_site/controller/action
+       header("Location:".$rootWeb.$controller."/".$action);
+       
+      }
 
      
  }
