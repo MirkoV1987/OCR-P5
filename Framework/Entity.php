c766d1abbe61edc56b4b3e90ef8578ed65dbc77e
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-16 23:10:20 +0200

    Upload Entity
    Upload Entity class

---
+++ Framework/Entity.php
@@ -0,0 +1,25 @@
+<?php
+
+//On écrit une classe abstraite qui hydrate les classes filles
+
+abstract class Entity
+{
+
+    public function __construct(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach($data as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+
+            if(method_exists($this, $method))
+               $this->$method($value);
+        }
+
+    }
+
+}
