5dcd090e21d5af3f6808324fd55417ca28000fd1
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-16 23:09:06 +0200

    Upload Model
    Abstract class Model

---
+++ Framework/Model.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class Model 
+{
+
+    private static $db;
+    
+    private static function setDb()
+    {
+            self::$db = new \PDO('mysql:host=localhost;dbname=dbblog;charset=utf8', 'root', '');
+            self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+    }
+
+    protected function getDb()
+    {
+        if(self::$db == null)
+           self::setDb();
+        return self::$db;
+    }
+
+}
