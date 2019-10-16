3c902b8c24e7297d3f2bdde89c586837fae5fdc2
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-16 23:17:17 +0200

    Create Post.php
    Upload Post model

---
+++ Model/Post.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * Cette classe hydrate les entités de type Post
+ * Cette classe utilise les méthodes de la classe mère Entity
+ */
+
+require_once("Framework/Entity.php");
+
+class Post extends Entity
+{
+
+    private $id;
+    private $title;
+    private $chapeau;
+    private $date_add;
+    private $date_update;
+    private $user_id;
+    private $content;
+
+    //SETTERS
+    public function setId($id)
+    {
+        $id = (int) $id;
+        if($id > 0)
+           $this->id = $id;
+    }
+
+    public function setTitle($title)
+    {
+        $title = (string) $title;
+        if(is_string($title))
+           $this->title = $title;
+    }
+
+    public function setChapeau($chapeau)
+    {
+        $chapeau = (string) $chapeau;
+        if(is_string($chapeau))
+           $this->chapeau = $chapeau;
+    }
+
+    public function setDate_add($date_add)
+    {
+           $this->date_add = $date_add;
+    }
+
+    public function setDate_update($date_update)
+    {
+           $this->date_update = $date_update;
+    }
+
+    public function setUser_id($user_id)
+    {
+        $user_id = (int) $user_id;
+        if($user_id > 0)
+           $this->user_id = $user_id;
+    }
+
+    public function setContent($content)
+    {
+        $content = (string) $content;
+        if(is_string($content))
+           $this->content = $content;
+    }
+
+    //GETTERS
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getChapeau()
+    {
+        return $this->chapeau;
+    }
+
+    public function getDate_add($lg = "")
+    {
+        // return date("d/m/Y h:m", strtotime($this->date_add));
+        return $this->date_add;
+           
+
+    }
+
+    public function getDate_update()
+    {
+        return $this->date_update;
+    }
+
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+    
+}
