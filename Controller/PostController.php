4376bdccd82471b958407cd478ebf1fb652fd719
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-16 23:14:41 +0200

    Create PostController
    Upload PostController

---
+++ Controller/PostController.php
@@ -0,0 +1,54 @@
+<?php 
+
+require_once('Manager/PostManager.php');
+//require_once('Framework/Controller.php');
+
+class PostController //extends Controller
+{
+   
+   private $manager;
+   private $view; 
+
+
+   public function __construct()
+   {
+      $this->manager = new PostManager;
+   }  
+
+   public function index()
+   {
+      $posts = $this->manager->getList();
+      require_once("View/homeView.php");
+   }
+
+   public function view($params)
+   {
+      //print_r($params);
+      $extract = explode('-', $params['get'][0]);
+      //print_r($extract);
+      $postId = intval($extract[0]);
+      $post = $this->manager->getPost($postId);
+      require_once("View/postView.php");
+   }
+
+   public function add($params)
+   {
+      print_r($params);
+      $post = $this->manager->add($params);
+      require_once("View/homeView.php");
+   }
+
+   public function update()
+   {
+      $post = $this->manager->update();
+      require_once("View/homeView.php");  
+   }
+
+   public function delete($params)
+   {
+      print_r($params);
+      $post = $this->manager->delete($params);
+      require_once("View/homeView.php");
+   }
+
+}   
