08f2e46a973d63633e521b22e943ab40855e7f53
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-21 16:06:36 +0200

    Update CommentManager
    Update getList, add and delete functions

--- Manager/CommentManager.php
+++ Manager/CommentManager.php
@@ -5,62 +5,53 @@ require_once('Framework/Model.php');
 require_once('Model/Comment.php');
 require_once('Framework/Entity.php');
 
-
 class CommentManager extends Model 
 {    
 
     public function getList()
     {
-        $commentsTab = [];
-        $req = $this->getDb()->prepare("SELECT id, title, chapeau, date_add, content FROM  post  ORDER BY id desc");
+        $comments = [];
+        $req = $this->getDb()->prepare("SELECT id, pseudo, content, date_add, active FROM comment ORDER BY id desc");
         $req->execute();
         while ($data = $req->fetch(\PDO::FETCH_ASSOC))
         {
-            array_push($commentsTab, new Comment($data));
+            array_push($comments, new Comment($data));
         }
-        return $commentsTab;
+        return $comments;
     }
 
     public function getComment($commentId) 
     {
         $comment = []; 
-        $req = $this->getDb()->prepare("SELECT id, title, chapeau, DATE_FORMAT(date_add, '%d/%m/%Y à %Hh%imin%ss') AS date_add_fr, DATE_FORMAT(date_update, '%d/%m/%Y à %Hh%imin%ss') AS date_update_fr, user_id, content FROM post WHERE id = :id");
-        $req->bindValue(':id', $postId, \PDO::PARAM_INT);
+        $req = $this->getDb()->prepare("SELECT id, pseudo, content, DATE_FORMAT(date_add, '%d/%m/%Y à %Hh%imin%ss') AS date_add_fr, post_id, active FROM comment WHERE id = :id");
+        $req->bindValue(':id', $commentId, \PDO::PARAM_INT);
         $req->execute();
         $req->setFetchMode(\PDO::FETCH_ASSOC);
         $comment = $req->fetch();
         return $comment;
     }
 
-    public function add($postId)
+    public function add($params)
     {
-        $post = new Comment($params['post']);
-        $req = $this->getDb()->prepare("INSERT INTO post(title, chapeau, date_add, content) VALUES(:title, :chapeau, :date_add, :content)");
-        $post = $req->execute(array(':title'=>$post->getTitle(), ':chapeau'=>$post->getChapeau(), ':date_add'=>$post->getDate_add(), ':content'=>$post->getContent() ));
-        return $post;
+        $comment = new Comment($params['post']);
+        $req = $this->getDb()->prepare("INSERT INTO comment(pseudo, content, date_add, active) VALUES(:pseudo, :content, :date_add, :active)");
+        $comment = $req->execute(array(':pseudo'=>$comment->getPseudo(), ':content'=>$comment->getContent(), ':date_add'=>date("Y-m-d H:i:s"), ':active'=>$comment->getActive() ) );
+        return $comment;
     }
 
-    public function update()
+    public function update($params)
     {
-        if(isset($_GET['update'])) {
-
-        $req = $this->getDb()->prepare("UPDATE post SET(title = :title, chapeau = :chapeau, date_update_fr = :date_update_fr, content = :content) WHERE(id = :id, user_id = :user_id)");
-        $post = $this->execute($req, array($post->getTitle(), $post->getChapeau(), $post->getDate_update(), $post->getContent()));
-
-        }
-        
+        $comment = new Comment($params['post']);
+        $req = $this->getDb()->prepare("UPDATE comment SET id = :id, pseudo = :pseudo, content = :content, date_update = :date_update WHERE id = :id");
+        $comment = $req->execute(array(':id'=>$comment->getId(), ':pseudo'=>$comment->getPseudo(), ':content'=>$comment->getContent(), ':date_update'=>date("Y-m-d H:i:s") ) );
+        return $comment;
     }
 
     public function delete($params)
     { 
-        $comment = new Comment($params['post']);
-        $req = $this->getDb()->prepare('DELETE FROM post WHERE id = :id');
-        //$req->bindValue(':id', $postId, \PDO::PARAM_INT);
-        $comment = $req->execute(array(':id'=>$post->getId() ) );
-        
-        print_r($comment);
+        $req = $this->getDb()->prepare('DELETE FROM comment WHERE id = :id');
+        $comment = $req->execute(array(':id'=>$params['get'][0]) );   
         return $comment;
-        
     }
     
 
