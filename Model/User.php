fedd1b77c342c974e61509c56705ddfb7efc8d9a
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-24 18:09:28 +0200

    Update User.php
    Add email private function, getters and setters properly.

--- Model/User.php
+++ Model/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace \Model;
+use Model;
 
 /**
  * Cette classe hydrate les entités de type User
@@ -12,20 +12,18 @@ class User
 
     private $id;
     private $username;
+    private $email;
     private $password;
     private $active;
     private $validation_key;
     private $role;
     private $date_add;
 
+    //GETTERS
+
     public function getId()
     {
       return $this->id;
-    }
-
-    public function setId(int $id)
-    {
-      $this->id = $id;
     }
 
     public function getUsername()
@@ -33,9 +31,9 @@ class User
       return $this->username;
     }
 
-    public function setUsername(string $username)
+    public function getEmail()
     {
-      $this->content = $username;
+      return $this->email;
     }
 
     public function getPassword()
@@ -43,19 +41,9 @@ class User
       return $this->password;
     }
 
-    public function setPassword(binary $password)
-    {
-      $this->password = $password;
-    }
-
     public function getActive()
     {
       return $this->active;
-    }
-
-    public function setActive(tinyint $active)
-    {
-      $this->active = $active;
     }
 
     public function getValidation_key()
@@ -63,25 +51,52 @@ class User
       return $this->validation_key;
     }
 
-    public function setValidation_key(binary $validation_key)
-    {
-      $this->validation_key = $validation_key;
-    }
-
     public function getRole()
     {
       return $this->role;
+    }
+
+    public function getDate_add($lg = "")
+    {
+      return date("d/m/Y h:m", strtotime($this->date_add));
+    }
+
+    //SETTERS
+
+    public function setId(int $id)
+    {
+      $this->id = $id;
+    }
+
+    public function setUsername(string $username)
+    {
+      $this->username = $username;
+    }
+
+    public function setEmail(string $email)
+    {
+      $this->email = $email;
+    }
+
+    public function setPassword(binary $password)
+    {
+      $this->password = $password;
+    }
+
+    public function setActive(tinyint $active)
+    {
+      $this->active = $active;
+    }
+
+    public function setValidation_key(binary $validation_key)
+    {
+      $this->validation_key = $validation_key;
     }
 
     public function setRole(int $role)
     {
       $this->role = $role;
     }  
-
-    public function getDate_add()
-    {
-      return $this->date_add;
-    }
 
     public function setDate_add(string $date_add)
     {
