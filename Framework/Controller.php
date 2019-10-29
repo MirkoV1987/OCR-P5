f67b9ff6255c03d133c7c04fc6e50f7ebc5dfb95
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-10-29 13:28:34 +0100

    Update abstract class Controller
    Add argument $params into index function

--- Framework/Controller.php
+++ Framework/Controller.php
@@ -1,51 +1,80 @@
-<?php   
-   
-use \Framework;
+<?php
 
-//require("Router.php");
+require_once('View.php');
+require_once('Request.php');
+
+/**
+ * Classe abstraite Controleur
+ * Fournit des services communs aux classes Controleur dérivées
+ */
 
 abstract class Controller 
 {
-     
-    protected $action; // Action à réaliser      
-    protected $request; // Requête entrante
-        
-    // Mémorisation de la requête entrante
-    public function setRequest($request)
-    {
-        $this->$request = $request;
-    }
-        
-    // Action à réaliser
-    public function executeAction()
-    {
-        if (method_exists($this, $action)) // Verifie si une méthode qui se nomme $action existe dans la classe actuelle.
+	/** action à réaliser */
+	private $action;
+	/** Requête entrante */
+	protected $request;
+	
+	/** Définit la requête entrante
+	 *
+	 * @param Request $request Requête entrante
+	 */
+	public function setRequest(Request $request) 
+	{
+		$this->request = $request;
+	}
+	
+	/**
+     * Exécute l'action à réaliser.
+     * Appelle la méthode portant le même nom que l'action sur l'objet Controleur courant
+     * 
+     * @throws \Exception Si l'action n'existe pas dans la classe Controleur courante
+     */
+	public function executeAction($action)
+	{
+		if (method_exists($this, $action)) // Verifie si une méthode qui se nomme $action existe dans la classe actuelle.
 		{
 			$this->action = $action;
-			$this->{$this->action}(); // Appel de la méthode dont le nom est $action
+			$this->{$this->action}(array()); // Appel de la méthode dont le nom est $action
 		}
 		else 
 		{
 			$classController = get_class($this);
-			throw new \Exception("Action '$action' non définie dans la classe $classController");
+			throw new \Exception("action '$action' non définie dans la classe $classController");
 		}
-    }
-        
-    /*
-    Méthode abstraite correspondant à l'action par défaut
-    Oblige les classes dérivées à implémenter cette action par défaut
-    */
-    //public abstract function index();
-        
-    // Génère la vue associée au contrôleur courant
-    protected function generateView($dataView = array())
-    {
-        // Détermination du nom du fichier vue à partir du nom du contrôleur actuel
-        $classController = get_class($this);
-        // instanciation de la vue
-        $view = new View($classController);
-        // lancement de la vue
-        $view->generate($dataView);
-    }
-        
+	}
+	/**
+     * Méthode abstraite correspondant à l'action par défaut
+     * Oblige les classes dérivées à implémenter cette action par défaut
+     */
+	public abstract function index($params);
+	/**
+     * Génère la vue associée au contrôleur courant
+     * 
+     * @param array $dataView Données nécessaires pour la génération de la vue
+     */
+	
+	protected function generateView($dataView = array()) 
+	{
+		// Détermination du nom du fichier vue à partir du nom du contrôleur actuel
+		$classController = get_class($this);
+		$controller = str_replace("Controller", "", $classController);
+		// Instanciation et génération de la vue
+		$view = new View($this->action, $controller);
+		$view->generate($dataView);
+	}
+
+	/**
+	 * Effectue une redirection vers un contrôleur et une action spécifiques
+	 * 
+	 * @param string $controller Controller
+	 * @param string $action action action
+	 */
+	protected function redirect($controller, $action = null)
+	{
+        //$rootWeb = Configuration::get("rootWeb", "/");
+        $rootWeb = 'http://hostmirko/OCR-P5/';
+	    // Redirection vers l'URL racine_site/controlller/action
+	    header("Location:" . $rootWeb . $controller . "/" . $action);
+	}
 }
