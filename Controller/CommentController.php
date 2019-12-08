eea0a0496bce262cf4c046eb875b7a30cb07c7ab
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-12-08 12:37:00 +0100

    Update CommentController
    Refactoring Code. Insert redirect and render functions (View)

--- Controller/CommentController.php
+++ Controller/CommentController.php
@@ -1,7 +1,10 @@
 <?php 
 
 require_once('Manager/CommentManager.php');
+require_once('Manager/PostManager.php');
 require_once('Framework/Controller.php');
+require_once('Framework/View.php');
+require_once('Framework/Request.php');
 
 class CommentController extends Controller
 {
@@ -18,30 +21,87 @@ class CommentController extends Controller
    {
       if (empty($params)) {
 
-         $comments = $this->manager->getList();
+      $comments = $this->manager->getList();
 
       } else {
 
-         $comments = $this->manager->getList($params['get'][0]); 
+      $comments = $this->manager->getList($params['get'][0]); 
 
       }
-      require_once("View/commentView.php");
-      //header('Location: /OCR-P5/comment/index');
+     
    }
 
    public function view($params)
    {
+      print_r($params);
       $extract = explode('-', $params['get'][0]);
-      $commentId = intval($extract[0]);
-      $comment = $this->manager->getComment($commentId);
-      require_once("View/CommentUpdate.php");
-      //header('Location: /OCR-P5/comment/view');
+      $post_id = intval($extract[0]);
+      $comments = $this->manager->getComment($post_id);
+      $this->set('comments', $comments);
+      $this->set('post', $post);
+
+      $user = $_SESSION['user']['role'];
+
+      if (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] == 1 || $_SESSION['user']['role'] == 2) {
+
+         $comments = $this->manager->getComment($post_id);
+         $this->set('comments', $comments);
+         $this->set('post', $post);
+
+         $this->render('view/post/connectedView.php');
+
+      }
+
+      // if (empty($params['get'][0]) || $params['active'] == 0 ) {
+
+      //    header('location: /OCR-P5/view/comment/view.php');
+          
+      // } 
+
+      
+      $this->render('view/post/view.php');
+      
    }
 
    public function add($params)
    {
-      $comment = $this->manager->add($params);
-      $view = $this->executeAction("index");
+      if (!empty($params['post']) && isset($_SESSION['user']['role']) ) {
+
+         if ($_SESSION['user']['role'] == 1) {
+
+         $comment = $this->manager->add($params);
+         $comment = $this->manager->add($params);
+         $post_id = $params['get'][0]; 
+         $this->redirect('/OCR-P5/post/view/'.$post_id);
+         //exit;
+         }
+
+         if ($_SESSION['user']['role'] == 2) {
+
+         $comment = $this->manager->add($params);
+         $post_id = $params['get'][0]; 
+         $this->redirect('/OCR-P5/post/view/'.$post_id);
+  
+         }
+      
+      }
+
+      $this->redirect('/OCR-P5/post/view/'.$post_id);
+
+   }
+
+   public function validate($params)
+   {
+      if ($_SESSION['user']['role'] == 2) {
+
+      $comment = $this->manager->validate($params);
+      $post_id = $params['get'][0];
+      $this->redirect('/OCR-P5/admin/index');
+
+      }
+
+      $this->redirect('OCR-P5/');
+
    }
 
    public function update($params)
@@ -50,10 +110,16 @@ class CommentController extends Controller
       $view = $this->executeAction("index");
    }
 
+
    public function delete($params)
    {
+      if($_SESSION['user']['role'] != 2) {
+
+         header('location: /OCR-P5/post');
+ 
+      }
       $comment = $this->manager->delete($params);
-      $view = $this->executeAction("index");
+      $view = $this->redirect('/OCR-P5/admin');
    }
 
-} 
+}   
