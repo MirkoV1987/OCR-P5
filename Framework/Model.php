53c4fa028f4b33a0e5e22dc4de9fedd2a56252cd
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2019-12-13 23:19:43 +0100

    Update Model
    Syntax Changes

--- Framework/Model.php
+++ Framework/Model.php
@@ -4,20 +4,19 @@ namespace Framework;
 
 abstract class Model 
 {
-
     private static $db;
     
     private static function setDb()
     {
-            self::$db = new \PDO('mysql:host=localhost;dbname=dbblog;charset=utf8', 'root', '');
-            self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+     self::$db = new \PDO('mysql:host=localhost;dbname=dbblog;charset=utf8', 'root', '');
+     self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
     }
 
     protected function getDb()
     {
-        if(self::$db == null)
-           self::setDb();
-        return self::$db;
+     if(self::$db == null)
+        self::setDb();
+     return self::$db;
     }
 
 }
