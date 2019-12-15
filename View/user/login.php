8779a7362e29e4c736b5dec06bae209efc98f927
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-12-15 11:26:53 +0100

    Upload login.php

--- View/user/login.php
+++ View/user/login.php
@@ -29,7 +29,9 @@
 <body class="bg-gradient-primary">
 
 <div class="container">
-  <div class="row">
+  <div class="row"> 
+
+
     <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
@@ -67,46 +69,37 @@
   </nav>
 </div>
 
-  <!-- Outer Row -->
-  <div class="row justify-content-center">
-    <div class="col-xl-10 col-lg-12 col-md-9">
-      <div class="card o-hidden border-0 shadow-lg my-5">
-        <div class="card-body p-0">
-          <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-            <div class="col-lg-6">
-          <div class="p-5">
-          <div class="text-center">
+<!-- Outer Row -->
+        <div class="row justify-content-center">
+          <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+              <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                  <div class="row">
+                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                  <div class="col-lg-6">
+                <div class="p-5">
+              <div class="text-center">
             <h1 class="h4 text-gray-900 mb-4">Login</h1>
-          </div>
-        <form class="user" action="login" method="post">
-          <div class="form-group">
-            <input type="username" name="username" class="form-control form-control-user" id="username" placeholder="Utilisateur" required>
-          </div>
-          <div class="form-group">
-            <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="Password" required>
-          </div>
-          <div class="form-group">
-            <div class="custom-control custom-checkbox small">
-              <input type="checkbox" class="custom-control-input" id="customCheck">
-              <label class="custom-control-label" for="customCheck">
-                Se souvenir
-              </label>
             </div>
-          </div>
-          <button type="submit" name="submit" id="login" value="connexion" class="btn btn-primary btn-user btn-block">Connexion</button>
-        </form>
-          <hr>
-            <!-- <div class="text-center">
-              <a class="small" href="forgot-password.html">Password oublié ?</a>
-            </div> -->
+            <form class="user" action="login" method="post">
+              <div class="form-group">
+                <input type="username" name="username" class="form-control form-control-user" id="username" placeholder="Utilisateur" required>
+              </div>
+              <div class="form-group">
+                <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="Password" required>
+              </div>
+              <button type="submit" name="submit" id="login" value="connexion" class="btn btn-primary btn-user btn-block">Connexion</button>
+            </form>
+            <hr>
             <div class="text-center">
               <a class="small" href="/OCR-P5/user/register/">Créer un compte!</a>
             </div>
           </div>
         </div>
       </div>
+
+  <!--End of the Main Content-->    
     </div>
   </div>
 
