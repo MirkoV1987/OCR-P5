eab81c0d28e5c99fddbccd19797ac2bb68991980
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-12-16 22:00:57 +0100

    Update Session Class

--- Framework/Session.php
+++ Framework/Session.php
@@ -2,13 +2,39 @@
 
 namespace Framework;
 
-abstract class Session
+class Session
 {
-	protected $session;
-	
-	public function Session(Session $session) 
-	{
-		$this->session = $_SESSION;
-	}
-	
+    public function __construct()
+    {
+        session_start();
+    }
+ 
+    public function destroy()
+    {
+        session_destroy();
+    }
+
+    public function setAttribute($name, $value)
+    {
+        $name = htmlspecialchars($name);
+        $value = htmlspecialchars($value);
+        $_SESSION[$name] = $value;
+    }
+  
+    public function existsAttribute($name)
+    {
+        return (isset($_SESSION[$name]) && $_SESSION[$name] != "");
+    }
+
+    public function getAttribute($name)
+    {
+        if ($this->existsAttribute($name)) 
+        {
+        return $_SESSION[$name];
+        }
+        else 
+        {
+        throw new \Exception("Attribut '$name' absent de la session");
+        }
+    }
 }
