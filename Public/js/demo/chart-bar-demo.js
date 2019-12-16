146a2cfcd17e876df0a57b3db15b1cda84257034
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-12-16 10:24:24 +0100

    Remove var Chart

--- Public/js/demo/chart-bar-demo.js
+++ Public/js/demo/chart-bar-demo.js
@@ -1,5 +1,4 @@
 // Set new default font family and font color to mimic Bootstrap"s default styling
-var Chart = new Chart;
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system' ,'system-ui', 'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica Neue','Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = "#858796";
 
