a8b37e5219eef866a93369cfd96968e6f9326aa5
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-12-17 22:29:59 +0100

    Update Model change database var name

--- Framework/Model.php
+++ Framework/Model.php
@@ -4,19 +4,19 @@ namespace Framework;
 
 abstract class Model 
 {
-    private static $db;
+    private static $database;
     
     private static function setDb()
     {
-     self::$db = new \PDO('mysql:host=localhost;dbname=dbblog;charset=utf8', 'root', '');
-     self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+     self::$database = new \PDO('mysql:host=localhost;dbname=dbblog;charset=utf8', 'root', '');
+     self::$database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
     }
 
     protected function getDb()
     {
-     if(self::$db == null)
+     if(self::$database == null)
         self::setDb();
-     return self::$db;
+     return self::$database;
     }
 
 }
