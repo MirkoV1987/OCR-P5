4b51648c568af79a1e7139f1f4a4581a10e3f463
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-12-18 00:25:55 +0100

    Update code style

--- Public/js/agency.js
+++ Public/js/agency.js
@@ -1,13 +1,13 @@
-(function($) {
+( (function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $("a.js-scroll-trigger[href*='#']:not([href='#'])").click(function() {
+    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
+        $("html, body").animate({
           scrollTop: (target.offset().top - 54)
         }, 1000, "easeInOutExpo");
         return false;
@@ -16,13 +16,13 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
+  $("body").scrollspy({
+    target: "#mainNav",
     offset: 56
   });
 
@@ -39,4 +39,4 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-})(jQuery); // End of use strict
+}) )(jQuery);  // End of use strict
