a1a28cbe1733a92b4507062a28cfa0d721b6d622
Mirko Venturi <mirkoventuri.web@gmail.com>
2019-12-23 21:20:40 +0100

    Update View

--- Framework/View.php
+++ Framework/View.php
@@ -4,40 +4,8 @@ namespace Framework;
 
 class View
 {
-    private $file;
-    private $title;
+
     private $vars = array();
-
-    public function __construct($action) 
-    {
-     $this->file = 'View/'.$view.'.php';
-    }
-
-    public function generate($data)
-    {
-        $content = $this->generateFile($this->file, $data);
-
-        $view = $this->generateFile('View/'.$folder.'/'.'template.php', array('title' => $this->title, 'content' => $content) );
-        
-        echo $view;
-    }
-
-    //Génère un fichier view et renvoie le résultat
-    private function generateFile($file, $data)
-    {
-          if (file_exists($file) )
-          {
-           extract($data);
-
-           ob_start();
-
-           require $file;
-
-           return ob_get_clean();
-          }
-          else
-               throw new Exception('Fichier '.$file.' introuvable');
-    }
 
     public function render($view)
     {
@@ -62,7 +30,7 @@ class View
     public function redirect($url)
     {
      header("Location: $url"); 
-     exit;
+     //exit;
     }
 
 }
