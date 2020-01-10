c0ade9f82bcec6cad4ea8ded6c3dc1f3b9ab1bda
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2020-01-10 13:22:50 +0100

    Create mail.php
    Contact Form Redirect Page

---
+++ View/mail.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Blog - Mirko Venturi</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/Public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="/Public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="/Public/css/email.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdn.3up.dk/in-view@0.6.1"></script>
+  <script src="/Public/js/email.js"></script>
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="xs-12 md-6 mx-auto">
+            <div id="countUp">
+                <div class="number" data-count="404">0</div>
+                <div class="text">Votre message a été envoyé dans l'espace !</div>
+                <div class="text">Il est en train de voyager pour atteindre de nouveaux horizons !</div>
+                <a class="btn rounded btn-md btn-warning text-center js-scroll-trigger my-1 text-uppercase text-white font-weight-bold" href="/#home">Revenir sur terre</a>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>    
+
+</body>
+
+</html>
