60c0285fe0c01ab1f850b6d51b1cf34bde93619d
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-12 14:16:18 +0100

    Add date_default_timezone_set function

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 require 'vendor/autoload.php';
 
+//Set Timezone
+if (function_exists("date_default_timezone_set") ) {
+
+    date_default_timezone_set("Europe/Paris");
+
+}
+
 \Framework\Session::getSession();
 
 $router = new \Framework\Router();
