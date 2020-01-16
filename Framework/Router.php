28ed07ba7da55a8ac19819ed9b65bf1cbebbf200
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-16 23:24:00 +0100

    Upload createController function, Error handling

--- Framework/Router.php
+++ Framework/Router.php
@@ -5,83 +5,52 @@ namespace Framework;
 use Controllers;
 
 class Router
-{ 
-    
+{
     public function routingRequest()
     {
-	    
-	try
-        {
-
+        try {
             $url = "";
          
-            if (isset($_GET['url']) ) {
-               
-            $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL) );
+            if (isset($_GET['url'])) {
+                $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
                 
-            $method = 'index';
+                $method = 'index';
 
-            if (isset($url[1]) ) {
-                  
-            $method = $url[1];
-
-         }
-		   
-            $params = [];
-
-            if (isset($url[2])) {
-                  
-            $params['get'] = array_slice($url, 2);
-
-             }
-               
-             if (isset($_POST)) {
-
-             $params['post'] = $_POST;
-
-             }
-        
-               $this->createController($url[0], $method, $params);   
+                if (isset($url[1])) {
+                    $method = $url[1];
+                }
            
-            }        
+                $params = [];
 
-            $this->createController('post','index', []);
+                if (isset($url[2])) {
+                    $params['get'] = array_slice($url, 2);
+                }
+               
+                if (isset($_POST)) {
+                    $params['post'] = $_POST;
+                }
+        
+                $this->createController($url[0], $method, $params);
+            }
 
-        }
-        catch(Exception $e)
-        {
+            $this->createController('post', 'index', []);
+        } catch (Exception $e) {
             $e->getMessage();
-            require_once('View/errorView.php');        
+            require_once('View/404.php');
         }
-
     }
 
     private function createController($class, $method, $params)
-    {   
-        $request = new Request($params); //on crée un nouvel objet Request
+    {
+        $controller = ucfirst(strtolower($class));
+        $classController = '\\Controllers\\'.$controller."Controller";
 
-           if ($request->existsParams('controller') ) {
-               
-            $controller = $request->getParams('controller');
-            $controller = ucfirst($controller);
-
-           }
-
-           $controller = ucfirst(strtolower($class));
-           $classController = '\\Controllers\\'.$controller."Controller";
-       
-           $control = new $classController;
-           $control->$method($params);
-
-           // }
-        
-            // else {
-            // header("HTTP/1.0 404 Not Found");  
-            // require_once('View/404.php'); //Créer HTML et include avec require_once
-            // exit; 
-
-            //}
-
-      }
-
-} 
+        if (class_exists($classController) ) {
+            $control = new $classController;
+            $control->$method($params);
+        } else {
+            require_once('View/404.php'); 
+            exit;
+        }
+    }
+}
