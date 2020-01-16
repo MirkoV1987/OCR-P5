da9f3ec5af27b029283dde1f8382766425882c5b
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-16 23:14:45 +0100

    Indent code to improve legibility

--- Framework/Entity.php
+++ Framework/Entity.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Framework;
+
 //On écrit une classe abstraite qui hydrate les classes filles
 
 abstract class Entity
 {
-
     public function __construct(array $data)
     {
         $this->hydrate($data);
@@ -13,14 +13,12 @@ abstract class Entity
 
     public function hydrate(array $data)
     {
-        foreach($data as $key => $value)
-        {
+        foreach ($data as $key => $value) {
             $method = 'set'.ucfirst($key);
 
-            if(method_exists($this, $method))
-               $this->$method($value);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
         }
-
     }
-
 }
