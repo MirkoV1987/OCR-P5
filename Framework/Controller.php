1f34819e3ad899d6b8e92c10e9d7fb9d173affbf
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-16 23:13:30 +0100

    Simplify Controller

--- Framework/Controller.php
+++ Framework/Controller.php
@@ -4,15 +4,5 @@ namespace Framework;
 
 abstract class Controller extends View
 {
-	protected $request;
-	
-	/** Définit la requête entrante
-	 *
-	 * @param Request $request Requête entrante
-	 */
-	public function setRequest(Request $request) 
-	{
-		$this->request = $request;
-	}
-	
+   
 }
