0edaba1a4c8fe18f6eca435a6c0fc1e3d09ae061
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-17 10:37:57 +0100

    PHP cs fixer Format File

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 require 'vendor/autoload.php';
 
 //Set Timezone
-if (function_exists("date_default_timezone_set") ) {
-
+if (function_exists("date_default_timezone_set")) {
     date_default_timezone_set("Europe/Paris");
-
 }
 
 \Framework\Session::getSession();
