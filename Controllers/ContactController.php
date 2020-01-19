d9466d87ffec88014a3b2257ee92e13e95156252
MirkoV1987 <52068274+MirkoV1987@users.noreply.github.com>
2020-01-19 16:26:41 +0100

    Update checkForm method
    Refactorise email fields

--- Controllers/ContactController.php
+++ Controllers/ContactController.php
@@ -24,17 +24,21 @@ class ContactController
 
         // Sécurisation des champs
         $name = strip_tags(htmlspecialchars($params['post']['name']));
-        $email_address = strip_tags(htmlspecialchars($params['post']['email']));
+        $email = strip_tags(htmlspecialchars($params['post']['email']));
         $phone = strip_tags(htmlspecialchars($params['post']['phone']));
         $message = strip_tags(htmlspecialchars($params['post']['message']));
         
         // Création du mail et envoi du message
-        $to = 'mirkoventuri.web@gmail.com';
-        $email_subject = "Blog de Mirko Venturi :  $name";
-        $email_body = "Vous avez reçu un message depuis le formulaire de contact du Blog.\n\n"."Détails :\n\nNom : $name\n\nEmail : $email_address\n\nTélephone : $phone\n\nMessage:\n$message";
-        $headers = "From: noreply@gmail.com\n"; // Utiliser noreply@yourdomain.com.
-        $headers .= "Répondre à : $email_address";
-        mail($to, $email_subject, $email_body, $headers);
+        $to      = 'mirkoventuri.web@gmail.com';
+        $subject = "Blog de Mirko Venturi :  $name";
+        $message = "Vous avez reçu un message depuis le formulaire de contact du Blog.\n\n"."Détails :\n\nNom : $name\n\nTélephone : $phone\n\nMessage:\n$message";
+        $headers = array(
+        'From' => $email,
+        'Reply-To' => 'noreply@mirko-venturi.com',
+        'X-Mailer' => 'PHP/' . phpversion()
+        );
+   
+        mail($to, $subject, $message, $headers);
 
         $this->sent->redirect('/View/mail.php');
     }
