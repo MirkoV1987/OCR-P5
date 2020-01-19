afaec41d57bd7357fa87faea402f846a49082083
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-19 12:12:46 +0100

    Refactorise setDb() for Security issue

--- Framework/Model.php
+++ Framework/Model.php
@@ -2,21 +2,25 @@
 
 namespace Framework;
 
-abstract class Model 
+abstract class Model
 {
     private static $database;
     
     private static function setDb()
     {
-     self::$database = new \PDO('mysql:host=localhost;dbname=dbblog;charset=utf8', 'root', '');
-     self::$database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+        $dsn = Configuration::get("dsn");
+        $login = Configuration::get("login");
+        $password = Configuration::get("password");
+
+        self::$database = new \PDO($dsn, $login, $password);
+        self::$database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
     }
 
     protected function getDb()
     {
-     if(self::$database == null)
-        self::setDb();
-     return self::$database;
+        if (self::$database == null) {
+            self::setDb();
+        }
+        return self::$database;
     }
-
 }
