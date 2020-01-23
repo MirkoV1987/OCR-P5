7159bf6f6ad5c5bc04ceca256ef14fc0e2893714
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-01-23 22:39:25 +0100

    Set site_path

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+//App Root
+$site_path = $_SERVER['DOCUMENT_ROOT'];
+$current_path = __FILE__;
+$current_path = str_replace('\\', '/', $current_path);
+$app_path = dirname(str_replace($site_path, '', $current_path));
+
+if (!defined('APP_DIR')) {
+    define('APP_DIR', $app_path);
+}
+
 require 'vendor/autoload.php';
 
 //Set Timezone
